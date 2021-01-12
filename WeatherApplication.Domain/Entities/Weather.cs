84134b8c8f64b3fe0505937ba0bae4788830fd14
Babatunde Afolabi <tundeafolabi@users.noreply.github.com>
2021-01-12 18:12:37 +0100

    Add project files.

---
+++ WeatherApplication.Domain/Entities/Weather.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WeatherApplication.Domain.Entities
+{
+    public class Weather
+    {
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(1000, ErrorMessage = "Max length is reached")]
+        public double MinTemperature { get; set; }
+        [Required]
+        [StringLength(1000, ErrorMessage = "Max length is reached")]
+        public double MaxTemperature { get; set; }
+
+    }
+}
---
+++ WeatherApplication.Domain/UseCases/GetWeatherInteractor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WeatherApplication.Domain.Entities;
+using WeatherApplication.Interfaces.UseCases;
+
+namespace WeatherApplication.Domain.UseCases
+{
+    internal interface IRequestHandler<in TRequest, out TResponse>
+    {
+        TResponse Handle(TRequest data);
+    }
+    public class GetWeatherInteractor : IRequestHandler<string,Weather>
+    {
+
+        private readonly IRepository _repository;
+
+        public GetWeatherInteractor(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+    }
+}
